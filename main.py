a25febb3c67079ad6bedd5b1104ef1bf76a7438c
ZakPorat <zakporat2023@gmail.com>
2024-03-28 17:31:17 -0700

    Created main function

---
+++ main.py
@@ -0,0 +1,17 @@
+import json
+from functions.chatModel import Model
+from functions.youtubeFunctions import Scraper
+
+with open('prompts/general.json', 'r') as file:
+  prompt_data = json.load(file)
+
+if __name__ == '__main__':
+  scraper = Scraper('https://www.youtube.com/watch?v=UBaJzkfW44U&t=22s')
+  file = scraper.get_video_transcript_to_file()
+  print(file)
+
+  model = Model(file)
+  index = model.load_index()
+  query_response = model.query(index, prompt=str(prompt_data), summary_of_data="Educational YouTube video.")
+
+  print(query_response)
